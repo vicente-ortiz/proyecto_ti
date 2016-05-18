84b4b91280c38efe9ed7ad9491b796876e1cd18a
vortiz <vortiz@alumnos.uai.cl>
2016-05-18 14:32:37 -0300

    Agregue campos obligatorios

--- agregar_proyectos.php
+++ agregar_proyectos.php
@@ -71,6 +71,7 @@ echo'<form action="guardarproyecto.php" method="post" enctype="multipart/form-da
 		<tr><td>Monto requerido: </td><td><input type="text" name="monto" />*</td></tr>
 		 <tr><td></td><td><p><input name=enviardatos type="submit" /></p></td><td></td></tr>
 		<tr><td></td><td> *Campos obligatorios </td></tr>
+		<tr><td></td><td> *Campos obligatorios </td></tr>
 		</table>';
 
 // Show the page footer
