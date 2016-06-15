991ba473b03a11f06b2b94847b57d271f6627f65
vortiz <vortiz@alumnos.uai.cl>
2016-06-15 16:33:26 -0300

    jhvjhvhjv

--- README.md
+++ README.md
@@ -5,17 +5,3 @@ Crowdfunding project is a site where people can submit their projects publicly s
 
 DOWNLOAD PROCESS
 First you must download and unzip file in C : \ xampp \ htdocs \ moodle \ Local , giving the name " proyecto_ti " finally to get started entering the site "http : // localhost / moodle / local / proyecto_ti / inicio.php " .
-
-DATABASE
-localhost->phpmyadmin->moodle->new->name table: mdl_add_project
-
-Then you proceed to fill the database with these columns
-
--name->varchar(100)
--category->varchar(100)
--phone->int(13)
--email->varchar(45)
--content->longtext
--needmoney->bigint(255)
--gatheredmoney->bigint(255)
--id->int(11)-> it must be auto_increment so you have to activate the A_I option
---
+++ validacion.php
@@ -0,0 +1,77 @@
+
+<html>
+<html>
+<head>
+<title>Proyecto GIR</title>
+</head>
+<body background="portada4.png">
+
+<tr>
+
+		<td><h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		CROWDFUNDING PARA PROYECTOS</h1></td>
+		
+		<td><h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		 </h4>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		
+		<a href='inicio.php'>INICIO</a></td>
+
+		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href='proyectos.php'>PROYECTOS</a></td>
+</tr>
+
+
+
+</body>
+</html>
+<?php
+// This file  is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package local
+ * @subpackage crowfounding
+ * @copyright 2012-onwards Vicente Ortiz <vortiz@alumnos.uai.cl>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+// Minimum for Moodle to work, the basic libraries
+require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
+
+
+// Moodle pages require a context, that can be system, course or module (activity or resource)
+$context = context_system::instance();
+$PAGE->set_context($context);
+
+// Check that user is logued in the course.
+require_login();
+
+// Page navigation and URL settings.
+$PAGE->set_pagelayout('incourse');
+
+
+
+//This message appear when the proyect its added to the database
+echo '<br><br><br><br><center><h2>¿Estas seguro que deseas realizar un aporte por $100.000?</h2></center>
+		<form name="boton_volver" action="inicio.php" method="POST">
+		';   
+
+// Here goes the content
+//echo 'Hello world';
+
+// Show the page footer
+echo $OUTPUT->footer();
+?>
